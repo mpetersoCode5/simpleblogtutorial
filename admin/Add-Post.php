6884d80c36d9bc4b6da2fa5541d14557779b552e
System Administrator <root@Michaels-iMac.local>
2014-12-30 20:21:21 -0500

    Updated Add-Post.php

--- admin/Add-Post.php
+++ admin/Add-Post.php
@@ -14,3 +14,58 @@
 	<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
 	
 </form>
+
+<script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+<script>
+	tinymce.init({
+		selector: "textarea",
+		plugins: [
+			"advlist autolink lists link image charmap print preview anchor",
+			"searchreplace visualblocks code fullscreen",
+			"insertdatetime media table contextmenu paste"
+		],
+		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft algincenter alignright alignjustify | bullist numlist outdent indent | link image"
+	});
+</script>
+	
+<?php
+	//if form has been submitted process it
+	if(isset($_POST['submit']))
+	{
+		$_POST = array_map( 'stripslashes', $_POST);
+		
+		//collect form data
+		extract($_POST);
+		
+		//very basic validation
+		if($postTitle == '')
+		{
+			$error[] = 'Please enter the title.';
+		}
+		
+		if($postDesc == '')
+		{
+			$error[] = 'Please enter the description.';
+		}
+		
+		if($postCont == '')
+		{
+			$error[] = 'Please enter the content.';
+		}
+	}
+	
+	if(!isset($error))
+	{
+		try {
+			
+			//insert into database
+			$stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate) VALUES (:postTitle, :postDesc, :postCont, :postDate)');
+			$stmt->execute(array(
+				':postTitle' => $postTitle,
+				':postDesc' => $postCont,
+				':postDate' => date('Y-m-d H:i:s')
+			));
+			
+		}
+	}
+?>
